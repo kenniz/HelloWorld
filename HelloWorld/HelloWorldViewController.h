30a45028fd18852e6a71b1848f49350a4dc8c2b9
kenniz <kennizchan@gmail.com>
2012-03-04 21:00:25 +0800

    Initial Commit

---
+++ HelloWorld/HelloWorldViewController.h
@@ -0,0 +1,13 @@
+//
+//  HelloWorldViewController.h
+//  HelloWorld
+//
+//  Created by kenniz on 3/4/12.
+//  Copyright (c) 2012 __MyCompanyName__. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface HelloWorldViewController : UIViewController
+
+@end
